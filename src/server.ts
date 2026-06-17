4bd6ed2b6a2933161ac113713549d19d7862d29d
Ashish <your@email.com>
2026-06-17 10:55:06 +0530

    serverfile change

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
-import app from "./app";
-require('dotenv').config();
-import connectDB from "./config/db";
+import express from "express";
+import cors from "cors";
 
-const PORT = process.env.PORT;
+const app = express();
 
-connectDB();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server Is Running");
+});
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
