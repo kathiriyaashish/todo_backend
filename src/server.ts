34cf1237b6ef23cd4173344b8edceb4d65430650
ashish <kathiriyaashish433@gmail.com>
2026-06-17 11:37:09 +0530

    Refactor server setup and remove unused imports

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,4 @@
-import express from "express";
-import cors from "cors";
-
-const app = express();
-
-app.use(cors(
-  {
-    origin: "https://todo-iota-nine-38.vercel.app",
-      methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-
-  }
-));
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Server Is Running");
-});
+import app from "./app";
 
 const PORT = process.env.PORT || 5000;
 
